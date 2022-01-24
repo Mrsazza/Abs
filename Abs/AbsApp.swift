f75f9427a17fac9316fe8b5d570b77cc9b63a174
Mrsazza <shahriarhossain147@gmail.com>
2022-01-25 03:17:21 +0600

    Initial Commit

---
+++ Abs/AbsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsApp.swift
+//  Abs
+//
+//  Created by Sazza on 25/1/22.
+//
+
+import SwiftUI
+
+@main
+struct AbsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
